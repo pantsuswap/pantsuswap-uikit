5158d8f4a629a71b80a8344a88853279fc00c183
cryptodude-org <83842104+cryptodude-org@users.noreply.github.com>
2021-06-05 22:53:32 +0200

    commit

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -26,7 +26,7 @@ export const lightColors: Colors = {
   tertiary: "#EFF4F5",
   text: "#452A7A",
   textDisabled: "#BDC2C4",
-  textSubtle: "#FF720D",
+  textSubtle: "#faa400",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {

3a8e8245ba507ba4da739fe5954de28974853ccc
cryptodude-org <83842104+cryptodude-org@users.noreply.github.com>
2021-06-05 20:54:31 +0200

    Update ResetCSS.tsx

--- src/ResetCSS.tsx
+++ src/ResetCSS.tsx
@@ -71,7 +71,7 @@ const ResetCSS = createGlobalStyle`
     box-sizing: border-box;
   }
   * {
-    font-family: 'Kanit', sans-serif;
+    font-family: 'Raleway', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
